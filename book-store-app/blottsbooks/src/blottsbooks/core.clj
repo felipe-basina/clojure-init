a54a6dbb9b4ff603fee063fa35c5031c4dbeeb12
fnascimento <felipe.basina@gmail.com>
2019-02-14 23:12:08 -0200

    Alterado script core para exibicao de mensagem inicial.

--- book-store-app/blottsbooks/src/blottsbooks/core.clj
+++ book-store-app/blottsbooks/src/blottsbooks/core.clj
@@ -1,7 +1,14 @@
 (ns blottsbooks.core
   (:gen-class))
 
+(comment
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
   (println "Hello, World!"))
+)
+(defn say-hello [what]
+  (println "Welcome to" what))
+
+(defn -main []
+  (say-hello "Blotts Books"))
