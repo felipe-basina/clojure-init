d5a1013b596b7ee9b72b612bccafe18206a60f3e
fnascimento <felipe.basina@gmail.com>
2019-02-16 14:41:51 -0200

    Adicionado testes para adicionar elementos no inicio de uma lista.

--- init-scripts/playing-with-list.clj
+++ init-scripts/playing-with-list.clj
@@ -9,3 +9,11 @@
 (println "List's all element but first" (rest poems))
 
 (println "List value at nth (2) index" (nth poems 2))
+
+; The conj - conjuctor function when applied to list
+; add a new element as the first of it
+(println "Add new element as the first one with conj function" (conj poems "Jabberwocky"))
+
+; The cons - constructor function when applied to list
+; add a new element as the first of it
+(println "Add new element with cons function" (cons "Jabberwocky" poems))
