22fbd4eb827ced6f657f5167f4d1ab630d24a2b1
fnascimento <felipe.basina@gmail.com>
2019-02-16 17:21:49 -0200

    Adicionado exemplos para recuperar valores de um map.

--- init-scripts/first-map.clj
+++ init-scripts/first-map.clj
@@ -4,3 +4,12 @@
                         "published" "1838"))
 
 (println "Map" book-map)
+
+; Get value from a map with get function
+(println "Getting value from map with get function: 'title'" (get book-map "title"))
+
+; Get value from a map directly by key
+(println "Getting value from map with key 'title'" (book-map "title"))
+(println "Getting value from map with key 'author'" (book-map "author"))
+(println "Getting value from map with key 'published'" (book-map "published"))
+(println "Getting value from map with key 'copyright'" (book-map "copyright"))
