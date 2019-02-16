527a5a47653c6926e69315e0743f8782d9112ca5
fnascimento <felipe.basina@gmail.com>
2019-02-16 11:38:05 -0200

    Adicionado script para teste de criacao de lista.

---
+++ init-scripts/first-list.clj
@@ -0,0 +1,7 @@
+; For create a list we should provide a quote before parentheses: '([content])
+(println "Creating a list w/o function" '(1 "two" "3" 4.0 "five"))
+
+(println "Creation a list with function list" (list 1 2 3 "four" "five" 6))
+
+(def odd-numbers [1 3 5 7 9])
+(println "Adding values from vector to list" (list 0 odd-numbers "two" "four" 6))
