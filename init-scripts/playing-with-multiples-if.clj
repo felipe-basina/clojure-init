871a86148c6edc82b2cebf201f289eeeafa952ce
fnascimento <felipe.basina@gmail.com>
2019-03-04 09:10:11 -0300

    Adicionado script com multiplas condicoes.

---
+++ init-scripts/playing-with-multiples-if.clj
@@ -0,0 +1,13 @@
+(defn shipping-charge [preferred-customer order-amount]
+    (if preferred-customer
+        0.0
+        (if (< order-amount 50.0)
+            5.0
+            (if (< order-amount 100.0)
+                10.0
+                (* 0.1 order-amount)))))
+
+(println "Shipping charge for preferred customer" (shipping-charge true 150.0))
+(println "Shipping charge for order amount = 47.22" (shipping-charge false 47.22))
+(println "Shipping charge for order amount = 75.94" (shipping-charge false 75.94))
+(println "Shipping charge for order amount = 224.45" (shipping-charge false 224.45))
