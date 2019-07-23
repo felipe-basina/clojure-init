c5c5c56a1ceeb8e7a60b24bcadea8f8265432e29
fnascimento <felipe.basina@gmail.com>
2019-07-22 23:10:29 -0300

    Adicionado projeto exemplo api rest

---
+++ rest-demo/src/rest_demo/core.clj
@@ -0,0 +1,78 @@
+(ns rest-demo.core
+  (:require [org.httpkit.server :as server]
+            [compojure.core :refer :all]
+            [compojure.route :as route]
+            [ring.middleware.defaults :refer :all]
+            [clojure.pprint :as pp]
+            [clojure.string :as str]
+            [clojure.data.json :as json])
+  (:gen-class))
+
+; my people-collection mutable collection vector
+(def people-collection (atom []))
+
+;Collection Helper functions to add a new person
+(defn addperson 
+  ([firstname] (addperson firstname "default"))
+  ([firstname surname] (swap! people-collection conj {:firstname (str/capitalize firstname) :surname (str/capitalize surname)})))
+
+; Example JSON objects
+(addperson "Functional" "Human")
+(addperson "Mickey" "Mouse")
+
+; Return List of People
+(defn people-handler [req]
+        {:status  200
+         :headers {"Content-Type" "text/json"}
+         :body    (str (json/write-str @people-collection))})
+
+; Get the parameter specified by pname from :params object in req
+(defn getparameter [req pname] (get (:params req) pname))
+
+; Add a new person into the people-collection
+(defn addperson-handler [req]
+        {:status  200
+         :headers {"Content-Type" "text/json"}
+         :body    (-> (let [p (partial getparameter req)]
+                        (pp/pprint req)
+                        (str (json/write-str (addperson (p :firstname) (p :surname))))))})
+
+; Simple Body Page
+(defn simple-body-page [req]
+  {:status  200
+   :headers {"Content-Type" "text/html"}
+   :body    "Hello World"})
+
+; request-example
+(defn request-example [req]
+     {:status  200
+      :headers {"Content-Type" "text/html"}
+      :body    (->>
+                (pp/pprint req)
+                (str "Request Object: " req))})
+
+(defn hello-name [req] ;(3)
+     {:status  200
+      :headers {"Content-Type" "text/html"}
+      :body    (->
+                (pp/pprint req)
+                (str "Hello " (:name (:params req))))})
+
+(defroutes app-routes
+  (GET "/" [] simple-body-page)
+  (GET "/request" [] request-example)
+  (GET "/hello" [] hello-name)
+  (GET "/people" [] people-handler)
+  (GET "/people/add" [] addperson-handler)
+  (POST "/people/add2" [] addperson-handler)
+  (route/not-found "Error, page not found!"))                
+
+(defn -main
+  "This is our main entry point"
+  [& args]
+  (let [port (Integer/parseInt (or (System/getenv "PORT") "3000"))]
+    ; Run the server with Ring.defaults middleware
+    (server/run-server (wrap-defaults #'app-routes site-defaults) {:port port})
+    ; Run the server without ring defaults
+    ;(server/run-server #'app-routes {:port port})
+    (println (str "Running webserver at http:/127.0.0.1:" port "/"))))
