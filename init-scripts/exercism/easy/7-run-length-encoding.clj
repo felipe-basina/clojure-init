5164d35f810a1ac2054511867d14831d731f37ba
Felipe Nascimento <felipe.nascimento@becommerce.com.br>
2020-01-29 08:19:39 -0300

    Removido :else e adicionado descricao para imprimir resultados.

--- init-scripts/exercism/easy/7-run-length-encoding.clj
+++ init-scripts/exercism/easy/7-run-length-encoding.clj
@@ -8,8 +8,7 @@
     (cond
         (= (count string) 0) (str compressed (get-total total) first-letter)
         (= (first string) first-letter) (compress (rest string) compressed (inc total) (first string))
-        (not (= (first string) first-letter)) (compress (rest string) (str compressed (get-total total) first-letter) 1 (first string))
-        :else ""))
+        (not (= (first string) first-letter)) (compress (rest string) (str compressed (get-total total) first-letter) 1 (first string))))
 
 (defn run-compress [original]
     (let [string (#(str/split %1 #"") original)
@@ -17,8 +16,9 @@
             remaining (rest string)]
         (compress remaining "" 1 first-letter)))
 
-(println (run-compress "WWWWWWWWWWWWBWWWWWWWWWWWWBBBWWWWWWWWWWWWWWWWWWWWWWWWB")) ; 12WB12W3B24WB
-(println (run-compress "AABCCCDEEEE")) ; 2AB3CD4E
+(println "compress: WWWWWWWWWWWWBWWWWWWWWWWWWBBBWWWWWWWWWWWWWWWWWWWWWWWWB <" 
+    (run-compress "WWWWWWWWWWWWBWWWWWWWWWWWWBBBWWWWWWWWWWWWWWWWWWWWWWWWB") ">") ; 12WB12W3B24WB
+(println "compress: AABCCCDEEEE <"(run-compress "AABCCCDEEEE") ">") ; 2AB3CD4E
 
 (defn parse-int [s]
     (try
@@ -29,12 +29,11 @@
     (if (= index total) repeated-value
         (repeat-value (inc index) total value (str repeated-value value))))
 
-(defn decompress [string total aux first-char decompressed]
+(defn decompress [string total partial first-char decompressed]
     (cond
         (= (count string) 0) (str decompressed (repeat-value 0 total first-char ""))
         (= (parse-int first-char) -1) (decompress (rest string) 1 "" (first string) (str decompressed (repeat-value 0 total first-char "")))
-        (not (= (parse-int first-char) -1)) (decompress (rest string) (parse-int (str aux first-char)) (str aux first-char) (first string) decompressed)
-        :else ""))
+        (not (= (parse-int first-char) -1)) (decompress (rest string) (parse-int (str partial first-char)) (str partial first-char) (first string) decompressed)))
 
 (defn run-decompress [original]
     (let [string (#(str/split %1 #"") original)
@@ -42,5 +41,5 @@
             remaining (rest string)]
         (decompress remaining 1 "" first-letter "")))
 
-(println (run-decompress "2AB3CD4E")) ; AABCCCDEEEE
-(println (run-decompress "12WB12W3B24WB")) ; WWWWWWWWWWWWBWWWWWWWWWWWWBBBWWWWWWWWWWWWWWWWWWWWWWWWB
+(println "decompress: 2AB3CD4E <" (run-decompress "2AB3CD4E") ">") ; AABCCCDEEEE
+(println "decompress: 12WB12W3B24WB <" (run-decompress "12WB12W3B24WB") ">") ; WWWWWWWWWWWWBWWWWWWWWWWWWBBBWWWWWWWWWWWWWWWWWWWWWWWWB
