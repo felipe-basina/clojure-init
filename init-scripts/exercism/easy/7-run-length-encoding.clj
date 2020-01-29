e6046a858b7f350a476cfb1e145acc92d98a7c70
fnascimento <felipe.basina@gmail.com>
2020-01-28 22:54:22 -0300

    Adicionado operacao para descompactar string.

--- init-scripts/exercism/easy/7-run-length-encoding.clj
+++ init-scripts/exercism/easy/7-run-length-encoding.clj
@@ -19,3 +19,28 @@
 
 (println (run-compress "WWWWWWWWWWWWBWWWWWWWWWWWWBBBWWWWWWWWWWWWWWWWWWWWWWWWB")) ; 12WB12W3B24WB
 (println (run-compress "AABCCCDEEEE")) ; 2AB3CD4E
+
+(defn parse-int [s]
+    (try
+        (Integer. (re-find #"[0-9]*" s))  
+        (catch Exception e -1)))
+
+ (defn repeat-value [index total value repeated-value]
+    (if (= index total) repeated-value
+        (repeat-value (inc index) total value (str repeated-value value))))
+
+(defn decompress [string total aux first-char decompressed]
+    (cond
+        (= (count string) 0) (str decompressed (repeat-value 0 total first-char ""))
+        (= (parse-int first-char) -1) (decompress (rest string) 1 "" (first string) (str decompressed (repeat-value 0 total first-char "")))
+        (not (= (parse-int first-char) -1)) (decompress (rest string) (parse-int (str aux first-char)) (str aux first-char) (first string) decompressed)
+        :else ""))
+
+(defn run-decompress [original]
+    (let [string (#(str/split %1 #"") original)
+            first-letter (first string)
+            remaining (rest string)]
+        (decompress remaining 1 "" first-letter "")))
+
+(println (run-decompress "2AB3CD4E")) ; AABCCCDEEEE
+(println (run-decompress "12WB12W3B24WB")) ; WWWWWWWWWWWWBWWWWWWWWWWWWBBBWWWWWWWWWWWWWWWWWWWWWWWWB
