7683073f4ccf2b64901934edfa5ba0f8c988a540
fnascimento <felipe.basina@gmail.com>
2020-02-13 20:50:42 -0300

    Adicionado isbns para testes.

--- init-scripts/exercism/easy/8-isbn-verifier.clj
+++ init-scripts/exercism/easy/8-isbn-verifier.clj
@@ -27,13 +27,15 @@
        (+ sum (* (first digits) (count digits))))))
 
 (defn is-valid-isbn? [isbn]
-  (let [digits (convert-digits (split-string isbn))]
-      (=
-       (rem (sum-up-digits digits 0) 11)
-       0)))
+  (let [digits (convert-digits (split-string isbn))
+        sum-up (sum-up-digits digits 0)]
+    (println isbn " > " sum-up)
+    (= (rem sum-up 11) 0)))
 
-(println (is-valid-isbn? "3-598-21508-8"))
-(println (is-valid-isbn? "3598215088"))
-(println (is-valid-isbn? "3-598-21507-X"))
-(println (is-valid-isbn? "359821507X"))
+(println (is-valid-isbn? "3-598-21508-8")) ; ISBN 10
+(println (is-valid-isbn? "3598215088")) ; ISBN 10
+(println (is-valid-isbn? "3-598-21507-X")) ; ISBN 10
+(println (is-valid-isbn? "359821507X")) ; ISBN 10
+(println (is-valid-isbn? "9781234567897")) ; ISBN 13
+(println (is-valid-isbn? "978123456789712345")) ; ISBN 13 + 5 DIGITS
 
