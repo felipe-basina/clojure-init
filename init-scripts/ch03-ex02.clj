e07f29ae442e242b03344fb0b87945012262e837
fnascimento <felipe.basina@gmail.com>
2020-04-06 18:16:12 -0300

    Adicionado exercicio 02.

---
+++ init-scripts/ch03-ex02.clj
@@ -0,0 +1,7 @@
+;; Write a function that takes a number and adds 100 to it.
+
+(defn adds-100 [number]
+  (+ number 100))
+
+(println "Add to 5 = " (adds-100 5))
+(println "Add to 1000 = " (adds-100 1000))
