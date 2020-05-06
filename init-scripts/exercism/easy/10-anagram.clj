d4bc9e6d5c6bdea2e6900ac00b53d5df734a7627
fnascimento <felipe.basina@gmail.com>
2020-05-05 21:34:14 -0300

    Refatoracao da solucao para correcao de logica.

--- init-scripts/exercism/easy/10-anagram.clj
+++ init-scripts/exercism/easy/10-anagram.clj
@@ -3,10 +3,14 @@
 ;; program should return a list containing "inlets" .
 (require '[clojure.string :as str])
 
-(defn update-map-letters [map-letters letter]
-  (let [found-letter? (not (nil? (get map-letters letter)))]
-    (if (= found-letter? true) (dissoc map-letters letter)
-        (assoc map-letters letter 1))))
+(defn update-map-letters [map-letters nested-map-key-source nested-map-key-lookup letter]
+  (let [key-letter (keyword letter)
+        found-letter-lookup? (not (nil? (get-in map-letters [nested-map-key-lookup key-letter])))]
+    (if (= found-letter-lookup? true) (update-in map-letters [nested-map-key-lookup] dissoc key-letter)
+        (let [found-letter-source? (not (nil? (get-in map-letters [nested-map-key-source key-letter])))]
+          (if (= found-letter-source? true) (assoc-in map-letters [nested-map-key-source key-letter] 
+                                                       (inc (get-in map-letters [nested-map-key-source key-letter])))
+              (assoc-in map-letters [nested-map-key-source key-letter] 1))))))
 
 (defn check-anagram [word candidate]
   (loop [map-letters {}
@@ -15,8 +19,8 @@
     (let [word-letter (first reference)
           candidate-letter (first candidate)]
       (if (empty? reference) map-letters
-          (let [map-letters (update-map-letters map-letters word-letter)
-                map-letters (update-map-letters map-letters candidate-letter)]
+          (let [map-letters (update-map-letters map-letters :ref :cand word-letter)
+                map-letters (update-map-letters map-letters :cand :ref candidate-letter)]
             (recur map-letters
                    (rest reference)
                    (rest candidate)))))))
@@ -30,8 +34,9 @@
         (if (< index total-candidates) (let [candidate (nth candidates index) 
                                              candidate-letters (str/split candidate #"")]
                                          (if (= word-total-letters (count candidate)) (let [map-letters (check-anagram word-letters candidate-letters)] 
-                                                                                        (if (empty? map-letters) (recur (inc index)
-                                                                                                                        (conj anagrams candidate))
+                                                                                        (if (and (empty? (get-in map-letters [:ref])) 
+                                                                                                 (empty? (get-in map-letters [:cand]))) (recur (inc index)
+                                                                                                                                               (conj anagrams candidate))
                                                                                             (recur (inc index)
                                                                                                    anagrams)))
                                              (recur (inc index)
@@ -39,3 +44,4 @@
             anagrams))))
 
 (println (anagram "listen" '("enlists" "google" "inlets" "banana" "lentis")))
+(println (anagram "nnaa" '("mbmb" "nana")))
