0a83340e2e6f58f6032c2a34b6cf16e6cfc532ce
fnascimento <felipe.basina@gmail.com>
2020-05-07 16:30:38 -0300

    Refatoracao para solucao com melhor desempenho.

--- init-scripts/rackerhank/circular-array-rotation.clj
+++ init-scripts/rackerhank/circular-array-rotation.clj
@@ -1,17 +1,14 @@
-(defn rotate [array times]
-  (loop [total 0
-         array array]
-    (if (= total times) array
-        (recur (inc total)
-               (vec (cons (last array) (take (dec (count array)) array)))))))
-
 (defn circularArrayRotation [a k queries]
-  (let [rotated-array (rotate a k)]
+  (let [elements-total (count a)
+        distance-from-origin (rem k elements-total)]
     (loop [queries queries
            array []]
       (if (empty? queries) array
-          (let [index (first queries)]
+          (let [index (first queries)
+                new-index (- index distance-from-origin)
+                new-index (if (< new-index 0) (+ new-index elements-total)
+                              new-index)]
             (recur (rest queries)
-                   (conj array (nth rotated-array index))))))))
+                   (conj array (nth a new-index))))))))
 
 (println (circularArrayRotation [1 2 3] 2 [0 1 2]))
