946fc9ea6d4f34d7ccf4ecdcf92a4ec187877bbc
fnascimento <felipe.basina@gmail.com>
2020-05-25 17:11:17 -0300

    Refatoracao da solucao para quebra de recordes.

--- init-scripts/hackerrank/breaking-the-records.clj
+++ init-scripts/hackerrank/breaking-the-records.clj
@@ -1,36 +1,38 @@
-(defn update-min-score? [new-score current-min-score]
-  (if (= current-min-score 0) new-score
-    (< new-score current-min-score)))
+(defn update-min-score? []
+  (fn [new-score current-min-score]
+    (or (= current-min-score 0)
+        (< new-score current-min-score))))
 
-(defn update-min-score [score current-min-score score-map]
-  (if (update-min-score? score current-min-score)
-    (let [current-total-score (:total-min-score score-map)]
-      (assoc score-map :min-score score :total-min-score (inc current-total-score)))
-    score-map))
+(defn update-max-score? []
+  (fn [new-score current-max-score]
+    (or (= current-max-score 0) (> new-score current-max-score))))
 
-(defn update-max-score? [new-score current-max-score]
-  (> new-score current-max-score))
-
-(defn update-max-score [score current-max-score score-map]
-  (if (update-max-score? score current-max-score) 
-    (let [current-total-score (:total-max-score score-map)]
-      (assoc score-map :max-score score :total-max-score (inc current-total-score)))
-    score-map))
+(defn update-score [func score current-score score-map keyword-type]
+  (let [total (str "total-" keyword-type)]
+    (if (func score current-score) 
+      (let [current-total-score ((keyword total) score-map)]
+        (assoc score-map (keyword (str keyword-type)) score (keyword total) (inc current-total-score)))
+      score-map)))
 
 (defn update-score-map [score score-map]
   (let [max-score (:max-score score-map)
         min-score (:min-score score-map)
-        score-map (update-max-score score max-score score-map)
-        score-map (update-min-score score min-score score-map)]
+        score-map (update-score (update-max-score?) score max-score score-map "max-score")
+        score-map (update-score (update-min-score?) score min-score score-map "min-score")]
     score-map))
 
+(defn first-is-zero? [scores]
+  (= (first scores) 0))
+
 (defn breakingRecords [scores]
-    (loop [scores scores
-           score-map {:max-score 0 :total-max-score -1 :min-score 0 :total-min-score -1}]
-      (println "score-map" score-map)
-      (if (empty? scores) [(:total-max-score score-map) (:total-min-score score-map)]
-          (recur (rest scores)
-                 (update-score-map (first scores) score-map)))))
+    (let [first-is-zero? (first-is-zero? scores)]
+      (loop [scores scores
+             score-map {:max-score 0 :total-max-score -1 :min-score 0 :total-min-score -1}]
+        (if (empty? scores) 
+          [(:total-max-score score-map) (if first-is-zero? 0 (:total-min-score score-map))]
+            (recur (rest scores)
+                   (update-score-map (first scores) score-map))))))
 
 (println (breakingRecords [10 5 20 20 4 5 2 25 1]))
 (println (breakingRecords [3 4 21 36 10 28 35 5 24 42]))
+(println (breakingRecords [0 9 3 10 2 20]))
