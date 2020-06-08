b7e026108b292acfb8b1e1bd23630420e56dfc8f
fnascimento <felipe.basina@gmail.com>
2020-06-07 21:36:29 -0300

    Atualizado versao final problema de reducao de string.

--- init-scripts/hackerrank/super-reduced-string.clj
+++ init-scripts/hackerrank/super-reduced-string.clj
@@ -11,30 +11,35 @@
     (subs string 0 2)
     (catch Exception e (subs string 0 1))))
 
-(defn check-reduced-string [reduced]
-  (if (empty? reduced) "Empty String"
-      (let [sliced-str (slice-in-2 reduced)
-            splitted (split-string sliced-str)]
-        (if (= (count splitted) 2)
-          (if (same-letter? splitted) "Empty String"
-              reduced)
-          reduced))))
+(defn check-reduced-string [reduced-string]
+  (if (empty? reduced-string) "Empty String"
+      reduced-string))
+
+(defn reduce-string [string reduced-string]
+  (loop [string string
+         reduced-string reduced-string
+         reduced? false]
+    (if (empty? string) (if (not reduced?) (check-reduced-string reduced-string)
+                            (reduce-string reduced-string ""))
+        (let [sliced-str (slice-in-2 string)
+              splitted (split-string sliced-str)]
+          (println "string" string "reduced-string" reduced-string "sliced-str" sliced-str)
+          (if (= (count splitted) 2)
+            (if (same-letter? splitted)
+              (recur (subs string 2 (count string))
+                     reduced-string
+                     true)
+              (recur (subs string 1 (count string))
+                     (str reduced-string (first splitted))
+                     reduced?))
+            (recur (subs string 1 (count string))
+                   (str reduced-string (first splitted))
+                   reduced?))))))
 
 (defn superReducedString [string]
-  (loop [string string
-         reduced-string ""]
-    (if (empty? string) (check-reduced-string reduced-string)
-      (let [sliced-str (slice-in-2 string)
-          splitted (split-string sliced-str)]
-      (if (= (count splitted) 2)
-        (if (same-letter? splitted)
-          (recur (subs string 2 (count string))
-                 reduced-string)
-          (recur (subs string 1 (count string))
-                 (str reduced-string (first splitted))))
-        (recur (subs string 1 (count string))
-               (str reduced-string (first splitted))))))))
+  (reduce-string string ""))
 
 (println (superReducedString "aaabccddd"))
 (println (superReducedString "aa"))
 (println (superReducedString "baab"))
+(println (superReducedString "mwkommokwmxjivkkvijxshscbbcshsgwdyqqydwgzpnlzzlnpzvfeaiiaefvyeqjccjqeymhqhiihqhmhaomkkmoahhddymmyddh"))
