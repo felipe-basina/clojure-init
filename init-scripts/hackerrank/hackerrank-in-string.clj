14df52d16a9bef448cf71efec985fbf61909ed04
fnascimento <felipe.basina@gmail.com>
2020-06-12 12:16:07 -0300

    Adicionado solucao final para verificar existencia de substring utilizando regex.

--- init-scripts/hackerrank/hackerrank-in-string.clj
+++ init-scripts/hackerrank/hackerrank-in-string.clj
@@ -1,40 +1,9 @@
-(require '[clojure.string :as s])
-
-(def HACKERRANK "hackerrank")
-
-(defn split-str [string]
-  (s/split string #""))
-
-(defn count-letter [letter]
-  (fn [reference]
-    (if (= (str reference) (str letter)) 1
-        0)))
-
-(defn count-occurrencies [reference-letter splited-string]
-  (reduce + (map (count-letter reference-letter) splited-string)))
-
-(defn hackerrank-letter-map []
-  (let [splited (split-str HACKERRANK)]
-    (loop [splited splited
-           letter-map {}]
-      (if (empty? splited) letter-map
-          (recur (rest splited)
-                 (assoc letter-map (first splited) (count-occurrencies (first splited) (split-str HACKERRANK))))))))
-
 (defn hackerrankInString [string]
-  (let [splited (split-str string)
-        letter-map (hackerrank-letter-map)]
-    (loop [letter-map letter-map]
-      (if (empty? letter-map) true
-          (let [pair (first letter-map)
-                letter (first pair)
-                amount (second pair)
-                total-occurrencies (count-occurrencies letter splited)]
-            (println "pair" pair "letter" letter "amount" amount "total-occurrencies" total-occurrencies)
-            (if (>= total-occurrencies amount) (recur (rest letter-map))
-                false))))))
+  (let [find-substring? (re-matches (re-pattern #"(.*)[h](.*)[a](.*)[c](.*)[k](.*)[e](.*)[r](.*)[r](.*)[a](.*)[n](.*)[k](.*)") string)]
+    (println find-substring?)
+    (if find-substring? "YES" "NO")))
 
-;(println (hackerrankInString "hereiamstackerrank"))
-;(println (hackerrankInString "hackerworld"))
-;(println (hackerrankInString "hhaacckkekraraannk"))
+(println (hackerrankInString "hereiamstackerrank"))
+(println (hackerrankInString "hackerworld"))
+(println (hackerrankInString "hhaacckkekraraannk"))
 (println (hackerrankInString "rhbaasdndfsdskgbfefdbrsdfhuyatrjtcrtyytktjjt"))
