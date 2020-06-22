a8c4b038ab9622654c21b3e798aa8b6df253f106
fnascimento <felipe.basina@gmail.com>
2020-06-22 10:36:24 -0300

    Refatoracao solucao para strings com pesos uniforme.

--- init-scripts/hackerrank/weighted-uniform-strings.clj
+++ init-scripts/hackerrank/weighted-uniform-strings.clj
@@ -9,23 +9,46 @@
   (inc (.indexOf alphabet character)))
 
 (defn character-existing-weights [current-character previous-character-map]
-  (previous-character-map current-character))
+  (let [total (previous-character-map current-character)]
+    (if total total 0)))
 
 (defn remove-character [character previous-character-map]
   (dissoc previous-character-map character))
+
+(defn add-character [character weight previous-character-map]
+  (assoc previous-character-map character weight))
 
 (defn find-value [value]
   (fn [value-to-find]
     (= value-to-find value)))
 
-(defn exists-value? [value-to-find queries]
+(defn value-exists? [value-to-find queries]
   (let [value-found (filterv (find-value value-to-find) queries)]
-    (if (empty? value-found) false true)))
+    (if (empty? value-found) "NO" "YES")))
+
+(defn same-query? [current-value query]
+  (= current-value query))
+
+(defn find-by-query [string-vector query]
+  (loop [string-vec string-vector
+         previous-character {}]
+    (if (empty? string-vec) "No"
+        (let [character (first string-vec)
+              current-character-weight (character-weight character)
+              character-weights (character-existing-weights character previous-character)]
+          (if (or (same-query? current-character-weight query)
+                  (same-query? (+ current-character-weight character-weights) query)) "Yes"
+            (recur (rest string-vec)
+                   (let [previous-character (add-character character (+ current-character-weight character-weights) {})]
+                     previous-character)))))))
 
 (defn weightedUniformStrings [s queries]
-  (loop [string-vec (split-string s)
-         previous-character {}
-         result []]
-    (if (empty? string-vec) result
-        (let [character (first string-vec)
-              character-weights (character-existing-weights character previous-character)]))))
+  (let [string-vec (split-string s)]
+    (loop [queries queries
+           result []]
+      (if (empty? queries) result
+          (recur (rest queries)
+                 (conj result (find-by-query string-vec (first queries))))))))
+
+(println (weightedUniformStrings "abccddde" [1 3 12 5 9 10]))
+(println (weightedUniformStrings "aaabbbbcccddd" [9 7 8 12 5]))
