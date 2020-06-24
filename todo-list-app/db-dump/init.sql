c724417c08b7bdfd5b2665472cf73953bd82329a
Felipe Nascimento <felipe.nascimento@becommerce.com.br>
2020-06-24 16:01:28 -0300

    Adicionado script para inserir registros iniciais.

--- todo-list-app/db-dump/init.sql
+++ todo-list-app/db-dump/init.sql
@@ -14,3 +14,7 @@ SET character_set_client = utf8;
 SET character_set_connection = utf8;
 SET character_set_results = utf8;
 SET collation_connection = utf8_general_ci;
+
+INSERT INTO todos (id, title, description, is_complete) values 
+(1, "To buy milk", "Need to buy milk after work", false),
+(2, "Schedule appointment about renting a car", "Renting a car when arriving at the airpoert", false);
