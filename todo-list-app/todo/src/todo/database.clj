522be168a6884819c499fd70f87ea811f6aaee0e
Felipe Nascimento <felipe.nascimento@becommerce.com.br>
2020-06-24 16:02:57 -0300

    Alterado arquivo para recuperar info de conexao com a base de dados atraves de variaveis de ambiente.

--- todo-list-app/todo/src/todo/database.clj
+++ todo-list-app/todo/src/todo/database.clj
@@ -2,10 +2,10 @@
   (:require [korma.db :as korma]))
 
 (def db-connection-data
-  (korma/mysql {:host "localhost" 
-                :port 3307 
-                :db "todolist" 
-                :user "root" 
-                :password "s3cr3t"}))
+  (korma/mysql {:host (System/getenv "DATABASE_HOST") 
+                :port (Integer/parseInt (System/getenv "DATABASE_PORT"))
+                :db (System/getenv "DATABASE_SCHEMA")
+                :user (System/getenv "DATABASE_USER") 
+                :password (System/getenv "DATABASE_PASSWORD")}))
 
 (korma/defdb conn db-connection-data)
