3ee0feb17f2ae1a02713bb2f3b711090f2e1cd7e
fnascimento <felipe.basina@gmail.com>
2020-06-25 11:49:11 -0300

    Adicionado exemplo de thread last macro.

---
+++ init-scripts/thread-last-macro.clj
@@ -0,0 +1,9 @@
+; Realiza a soma dos quadrados de números ímpares menores que 10
+; O valor do parâmetro é adicionado como ÚLTIMO PARÂMETRO de cada função
+(println (->> (range 10)
+              (filter odd?)
+              (map #(* % %))
+              (reduce +)))
+
+; Equivalente à seguinte chamada:
+(println (reduce + (map #(* % %) (filter odd? (range 10)))))
