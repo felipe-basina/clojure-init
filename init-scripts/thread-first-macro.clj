dc86bbb328cd38b8976b700303157feb6134a063
fnascimento <felipe.basina@gmail.com>
2020-06-25 11:51:02 -0300

    Adicionado comentario explicativo.

--- init-scripts/thread-first-macro.clj
+++ init-scripts/thread-first-macro.clj
@@ -1,6 +1,7 @@
 (def person {:name "Socrates" :age 39})
 
 ; Utiliza o parâmetro 'person' em cada uma das seguintes chamadas; o retorno de cada função é repassado como parâmetro para a próxima função
+; O valor do parâmetro é adicionado como PRIMEIRO PARÂMETRO de cada função
 (println (-> person
              (assoc :hair-color :gray)
              (update :age inc)))
