fef4a94819b27fc69968450de16cfc8f59f32d5b
fnascimento <felipe.basina@gmail.com>
2020-07-14 22:16:30 -0300

    Adicionado funcao para contabilizar o total de letras, de cada letra, de uma palavra.

---
+++ init-scripts/map-total-characters.clj
@@ -0,0 +1,26 @@
+;
+; Contabiliza o total de letras, de cada letra, de uma determinada palavra
+;
+(require '[clojure.string :as s])
+
+(defn same-character? [char]
+  (fn [char-for-compare]
+    (if (= char-for-compare char) 1 0)))
+
+(defn count-occurrencies [char s]
+  (reduce + (map (same-character? char) (s/split s #""))))
+
+(defn add-to-map [char-map char total]
+  (if (not (contains? char-map char)) (assoc char-map char total)
+      char-map))
+
+(defn map-of-characters [s]
+  (loop [str-vec (vec (s/split s #""))
+         char-map {}]
+    (if (empty? str-vec) char-map
+        (let [char (first str-vec)
+              total-of-occurrencies (count-occurrencies char s)]
+          (recur (rest str-vec)
+                 (add-to-map char-map char total-of-occurrencies))))))
+
+(println (map-of-characters "anagrama"))
