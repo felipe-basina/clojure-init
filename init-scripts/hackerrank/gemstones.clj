21695ccf5318174c51556d2b8127198644d4af9f
fnascimento <felipe.basina@gmail.com>
2020-07-16 18:51:28 -0300

    Refatoracao do codigo para uma versao mais performatica.

--- init-scripts/hackerrank/gemstones.clj
+++ init-scripts/hackerrank/gemstones.clj
@@ -4,7 +4,6 @@
   (s/includes? rock mineral))
 
 (defn gemstone? [mineral rocks]
-  ;(println "mineral" mineral "rocks" rocks)
   (loop [rocks rocks
          analyze? true]
     (if (or (empty? rocks)
@@ -15,24 +14,15 @@
           (recur []
                  false)))))
 
-(defn remove-same-minerals [current-mineral minerals]
-  (let [join (reduce str minerals)
-        updated-minerals (s/replace join current-mineral "")]
-    (println "join" join "updated-minerals" updated-minerals)
-    (s/split updated-minerals #"")))
-
 (defn gemstones [arr]
-  (let [first-rock (first arr)
-        minerals (s/split first-rock #"")
-        arr (rest arr)]
-    ;(println "first-rock" first-rock "minerals" minerals "(first minerals)" (first minerals))
+  (let [letters "abcdefghijklmnopqrstuvxzyw"
+        minerals (s/split letters #"")]
     (loop [minerals minerals
            total 0]
       (if (empty? minerals) total
           (let [gemstone? (gemstone? (first minerals) arr)
-                total-gemstones (if gemstone? (do (println (first minerals)) 1) 0)
-                minerals (if gemstone? (remove-same-minerals (first minerals) minerals) (rest minerals))]
-            (recur minerals
+                total-gemstones (if gemstone? (do (println (first minerals)) 1) 0)]
+            (recur (rest minerals)
                    (+ total total-gemstones)))))))
 
 (println (gemstones ["abcdde" "baccd" "eeabg"]))
