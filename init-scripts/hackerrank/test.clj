81ca60d218debe5df4fa2b0d83bb125c7e76fa5f
fnascimento <felipe.basina@gmail.com>
2020-07-16 21:38:37 -0300

    Testes para transformar faixa de caracteres em conjunto alfabetico.

--- init-scripts/hackerrank/test.clj
+++ init-scripts/hackerrank/test.clj
@@ -1,23 +1,9 @@
 (require '[clojure.string :as s])
 
-(defn same-character? [char]
-  (fn [char-for-compare]
-    (if (= char-for-compare char) 1 0)))
+(defn to-char [init end]
+  (map char (range (int init) (inc (int end)))))
 
-(defn count-occurrencies [char s]
-  (reduce + (map (same-character? char) (s/split s #""))))
-
-(defn add-to-map [char-map char total]
-  (if (not (contains? char-map char)) (assoc char-map char total)
-           char-map))
-
-(defn map-of-characters [s]
-  (loop [str-vec (vec (s/split s #""))
-        char-map {}]
-    (if (empty? str-vec) char-map
-        (let [char (first str-vec)
-              total-of-occurrencies (count-occurrencies char s)]
-          (recur (rest str-vec)
-                 (add-to-map char-map char total-of-occurrencies))))))
-
-(println (map-of-characters "anagrama"))
+(println (vec (to-char \a \z)))
+(println (apply str (vec (to-char \a \z))))
+(println (s/split (apply str (vec (to-char \a \z))) #""))
+(println (s/split "abcdefghijklmnopqrstuvwxyz" #""))
