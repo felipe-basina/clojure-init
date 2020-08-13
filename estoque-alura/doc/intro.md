4238522ccebace1d1db8f520fbe3cf96bd33b7ff
fnascimento <felipe.basina@gmail.com>
2020-08-13 20:35:17 -0300

    Adicionado projeto exemplo alura.

---
+++ estoque-alura/doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to estoque-alura
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ estoque-alura/test/estoque_alura/core_test.clj
@@ -0,0 +1,7 @@
+(ns estoque-alura.core-test
+  (:require [clojure.test :refer :all]
+            [estoque-alura.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
