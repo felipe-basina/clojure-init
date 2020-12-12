1bbc233d01cb5048eb76c2e657084de15c6d5fe0
felipe-basina <felipe.basina@gmail.com>
2020-12-12 16:36:51 -0300

    Adicionado testes de unidade para htpp post.

--- shouter/test/shouter/web_test.clj
+++ shouter/test/shouter/web_test.clj
@@ -2,7 +2,8 @@
   (:require [clojure.test :refer :all]
             [ring.mock.request :as mock]
             [shouter.web :refer :all]
-            [clojure.java.jdbc :as sql]))
+            [clojure.java.jdbc :as sql]
+            [ring.middleware.defaults :refer [wrap-defaults site-defaults]]))
 
 (deftest test-http-ok-for-root
   (testing "main route"
@@ -13,6 +14,38 @@
           (is (.contains (:body response) "What do you want to SHOUT?"))
           (is (.contains (:body response) "A mock message"))
           (is (.contains (:body response) "/1"))))))
+
+(deftest test-http-ok-for-delete
+  (testing "delete route"
+    (with-redefs [sql/delete! (fn [_ _ _] 1)
+                  sql/query (fn [_ _] [])]
+      (let [response (application (mock/request :get "/"))]
+        (is (= (:status response) 200))
+        (println response)
+        (is (.contains (:body response) "What do you want to SHOUT?"))))))
+
+; Disabling CSRF for testing
+(def app
+  (-> routes
+      (wrap-defaults (assoc site-defaults :security false))))
+
+(deftest test-http-ok-for-post
+  (testing "post route"
+    (with-redefs [sql/insert! (fn [_ _ _ _] 1)]
+      (let [response (app (mock/request :post "/" {:shout "Creating a message"}))]
+        (is (= (:status response) 302))))))
+
+;(deftest test-http-ok-for-post
+;  (testing "post route"
+;    (with-redefs [sql/insert! (fn [_ _ _ _] 1)
+;                  sql/query (fn [_ _] [{:id 101 :body "Creating a message" :created_at "2020-01-01 10:00:00"}])
+;                  ]
+;      (let [response (app (mock/request :post "/" {:shout "Creating a message"}))]
+;        (is (= (:status response) 302))
+;        (println response)
+;        (is (.contains (:body response) "What do you want to SHOUT?"))
+;        (is (.contains (:body response) "Creating a message"))
+;        (is (.contains (:body response) "/101"))))))
 
 (deftest test-http-not-found
   (testing "not-found route"
