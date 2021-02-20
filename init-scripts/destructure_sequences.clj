e9d0e324004e85755e0077573f77eb83a6574efe
felipe-basina <felipe.basina@gmail.com>
2021-02-20 12:57:08 -0300

    Adicionado exemplo de desestruturacao de elemento nao existente.

--- init-scripts/destructure_sequences.clj
+++ init-scripts/destructure_sequences.clj
@@ -1,7 +1,11 @@
 (def items ["jd" "Joe Doe" "Los Bagos"])
 
 ; Destructuring vector items to symbols
-(println (let [[alias name city] items]
+(println "All elements destructured?" (let [[alias name city] items]
               (= alias (nth items 0))
               (= name (nth items 1))
               (= city (nth items 2))))
+
+; When there are no elements to assign a value, the symbol will be nil
+(println "Is nil element?" (let [[alias name city country] items]
+              (nil? country)))
