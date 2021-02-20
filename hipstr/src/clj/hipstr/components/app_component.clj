480f3985dff1deb6695a5f48470a893004cf569a
felipe-basina <felipe.basina@gmail.com>
2021-02-20 16:35:56 -0300

    Adicionado metodo para verificar se chave existe no mapa da requisicao http.

--- hipstr/src/clj/hipstr/components/app_component.clj
+++ hipstr/src/clj/hipstr/components/app_component.clj
@@ -13,3 +13,16 @@
   (reduce merge
           (map create-map
                (split-by query-params "&"))))
+
+(defn render-request-val [request]
+  "Simply returns the value of request-key in request-map,
+  if request-key is provided; Otherwise return the request-map.
+  If request-key is provided, but not found in the request-map,
+  a message indicating as such will be returned."
+  (let [request-map request
+        request-key (:val (:path-params request))]
+    (str (if request-key
+         (if-let [result ((keyword request-key) request-map)]
+           result
+           (str request-key " is not a valid key."))
+         request-map))))
