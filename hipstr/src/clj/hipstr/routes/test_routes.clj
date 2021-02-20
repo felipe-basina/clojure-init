2cff2f304e81118a6cc939a4a58c3f129711aa37
felipe-basina <felipe.basina@gmail.com>
2021-02-20 15:59:45 -0300

    Adicionado rotas diversas para testes.

--- hipstr/src/clj/hipstr/routes/test_routes.clj
+++ hipstr/src/clj/hipstr/routes/test_routes.clj
@@ -4,7 +4,8 @@
     [clojure.java.io :as io]
     [hipstr.middleware :as middleware]
     [ring.util.response]
-    [ring.util.http-response :as response]))
+    [ring.util.http-response :as response]
+    [hipstr.components.app-component :as comp]))
 
 (defn print-request [request]
   (println "NEW ROUTE :: test-routes")
@@ -21,7 +22,13 @@
    :headers {"Content-Type" "text/html"}
    :body (:val (:path-params request))})
 
-(defn get-request-param-by-key2 [{:keys [:path-params :val] :as request}]
+(defn get-request-param-by-key1 [request]
+  (println "get-request-param-by-key1 the query-string" (:query-string request))
+  {:status 200
+   :headers {"Content-Type" "text/html"}
+   :body (str (comp/convert-query-param-to-map (:query-string request)))})
+
+(defn get-request-param-by-key2 [request]
   {:status 200
    :headers {"Content-Type" "text/html"}
    :body (:val (:path-params request))})
@@ -32,4 +39,5 @@
                  middleware/wrap-formats]}
    ["/req" {:get print-request}]
    ["/req/:val" {:get get-request-param-by-key}]
+   ["/req1" {:get get-request-param-by-key1}]
    ["/req2/:val" {:get get-request-param-by-key2}]])
