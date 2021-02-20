0e7f55262b12c88d54bde571df37d2e0985cf926
felipe-basina <felipe.basina@gmail.com>
2021-02-20 20:37:46 -0300

    Adicionado rotas para realizar o cadastro de usuario na aplicacao.

--- hipstr/src/clj/hipstr/routes/home.clj
+++ hipstr/src/clj/hipstr/routes/home.clj
@@ -4,7 +4,9 @@
    [clojure.java.io :as io]
    [hipstr.middleware :as middleware]
    [ring.util.response]
-   [ring.util.http-response :as response]))
+   [ring.util.http-response :as http-response]
+   [ring.util.response :as response]
+   [hipstr.components.signup :as signup]))
 
 (defn home-page [request]
   (layout/render request "home.html" {:docs (-> "docs/docs.md" io/resource slurp)}))
@@ -20,11 +22,26 @@
 (defn signup [request]
   (layout/render request "signup.html"))
 
+(defn signup-page-submit [request]
+  (let [errors (signup/validate-signup (:body request))]
+    (if (empty? errors)
+      (response/redirect "/signup-success")
+      (layout/render request "signup.html" {:errors errors}))))
+
+(defn success-signup [request]
+  (println "on success-signup" request)
+  {:status 200
+   :headers {"Content-Type" "text/html"}
+   :body "Success!"})
+
+;middleware/wrap-csrf
 (defn home-routes []
   [""
-   {:middleware [middleware/wrap-csrf
+   {:middleware [middleware/wrap-body-string
                  middleware/wrap-formats]}
    ["/" {:get home-page}]
-   ["/signup" {:get signup}]
+   ["/signup" {:get signup
+               :post signup-page-submit}]
+   ["/signup-success" {:get success-signup}]
    ["/about" {:get foo-response}]]) ; {:get about-page}
 
