a31b2b3f0d7f895279a3fcb092c84ece026ebfb9
felipe-basina <felipe.basina@gmail.com>
2021-02-20 15:49:53 -0300

    Adicionado funcao para imprimir conteudo de mapa.

--- init-scripts/query-params.clj
+++ init-scripts/query-params.clj
@@ -1,22 +1,21 @@
 (require '[clojure.string :as str])
 
-(def query-params "name=joe&last_name=doe")
-(println "query-params" query-params)
+(def query-params "name=joe&last_name=doe&age=21")
 
 (defn split-by [string split-value]
     (str/split string (re-pattern split-value)))
-
-(def splited-by-& (split-by query-params "&"))
-(println splited-by-& (count splited-by-&))
 
 (defn create-map [param]
     (let [splited (split-by param "=")]
         (assoc {} (keyword (first splited)) (second splited))))
 
-(def query-param-map (reduce merge (map create-map splited-by-&)))
-(println "query param as map:" query-param-map 
-    "\nis really a map?" (map? query-param-map)
-    "\nthe map keys:" (keys query-param-map)
-    "\nthe map values:" (vals query-param-map)
-    "\nthe :name key value is:" (:name query-param-map)
-    "\nthe :last_name key value is:" (:last_name query-param-map))
+(defn print-map-content [map-content]
+    (loop [content map-content]
+        (if (not-empty content)
+            (let [element (first content)]
+                (println "key" (first element) ", value" (second element))
+                (recur (rest content))))))
+
+(def query-param-map (reduce merge (map create-map (split-by query-params "&"))))
+(println "query param as map:" query-param-map "\nis really a map?" (map? query-param-map))
+(print-map-content query-param-map)
