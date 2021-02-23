1c7d9f84d6f305e6e6d0da506dd9986ee007edec
felipe-basina <felipe.basina@gmail.com>
2021-02-22 21:27:06 -0300

    Adicionado comentario + verificado do tipo de cada record.

--- init-scripts/exploring-records-and-protocols.clj
+++ init-scripts/exploring-records-and-protocols.clj
@@ -1,6 +1,9 @@
+; Set some records
 (defrecord FictionalCharacter [name appears-in author])
 (defrecord SuperComputer [cpu no-cpus storage-gb])
 
 (def watson-1 (->FictionalCharacter "John Watson" "Sign of the Four" "Doyle"))
 (def watson-2 (map->SuperComputer {:cpu "Power7" :no-cpus 2880 :storage-gb 4000}))
-(println "\twatson-1" watson-1 "\n\twatson-2" watson-2)
+(println "watson-1" watson-1 "\nwatson-2" watson-2)
+(println (class watson-1))
+(println (class watson-2))
