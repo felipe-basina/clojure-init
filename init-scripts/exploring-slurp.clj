9876275b4d68079c4cc36585eaac77ebd62c5f99
Felipe Nascimento <felipe-basina@users.noreply.github.com>
2021-03-02 09:14:36 -0300

    Adicionado exemplo de utilizacao de slurp.

---
+++ init-scripts/exploring-slurp.clj
@@ -0,0 +1,18 @@
+(require '[clojure.string :as str])
+
+(defn read-input
+  "Reads contents file at once and keep each of its lines in a vector.
+  Returns the vector"
+  []
+    (let [file-content (slurp "input.txt")
+          splited-content (str/split file-content #"\n")]
+       (loop [contents splited-content
+              new-content []]
+        (if (not-empty contents)
+            (recur (rest contents)
+                    (conj new-content (first contents)))
+            new-content))))
+
+(let [contents (read-input)]
+    (println "total: " (count contents))
+    (println "contents\n" contents))
