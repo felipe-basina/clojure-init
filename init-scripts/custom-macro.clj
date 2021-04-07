fc343589157132f328ceda5bcd59e76d19601619
Felipe Nascimento <felipe-basina@users.noreply.github.com>
2021-04-07 12:31:19 -0300

    Adicionado novos exemplos de macros.

--- init-scripts/custom-macro.clj
+++ init-scripts/custom-macro.clj
@@ -8,3 +8,25 @@
                ["Great cow of Moscow, this is good code:" good]])))
 
 (code-critic (1 + 1) (+ 1 1))
+
+(def message "Good job!")
+(defmacro with-mischief
+  [& stuff-to-do]
+  (println "stuff-to-do" stuff-to-do)
+  (concat (list 'let ['message "Oh, big deal!"])
+          stuff-to-do))
+
+(with-mischief
+  (println "Here's how I feel about that thing you did: " message))
+
+;; syntax symbol# applied for auto-generate symbols!
+(defmacro report
+  [to-try]
+  `(let [result# ~to-try]
+     (if result#
+       (println (quote ~to-try) "was successful:" result#)
+       (println (quote ~to-try) "was not successful:" result#))))
+
+(report (do (Thread/sleep 1000) (+ 1 1)))
+(report (= 1 1))
+(report (= 1 2))
