97656da5f8506b3085ada0a503cb7a22fba9ab38
felipe-basina <felipe.basina@gmail.com>
2021-06-14 20:32:59 -0300

    Adicionado exercicio sobre base de dados em memoria.

---
+++ init-scripts/sample_db.clj
@@ -0,0 +1,47 @@
+(ns sample-db)
+
+(def gemstone-db {
+                  :ruby {
+                         :name "Ruby"
+                         :stock 120
+                         :sales [1990 3644 6376 4918 7882 6747 7495 8573 5097 1712]
+                         :properties {
+                                      :dispersion 0.018
+                                      :hardness 9.0
+                                      :refractive-index [1.77 1.78]
+                                      :color "Red"
+                                      }
+                         }
+                  :diamond {
+                            :name "Diamond"
+                            :stock 10
+                            :sales [8295 329 5960 6118 4189 3436 9833 8870 9700 7182 7061 1579]
+                            :properties {
+                                         :dispersion 0.044
+                                         :hardness 10
+                                         :refractive-index [2.417 2.419]
+                                         :color "Typically yellow, brown or gray to colorless"
+                                         }
+                            }
+                  :moissanite {
+                               :name "Moissanite"
+                               :stock 45
+                               :sales [7761 3220]
+                               :properties {
+                                            :dispersion 0.104
+                                            :hardness 9.5
+                                            :refractive-index [2.65 2.69]
+                                            :color "Colorless, green, yellow"
+                                            }
+                               }
+                  }
+  )
+
+(defn durability [db gem]
+      (:hardness (:properties (gem db))))
+
+(defn durability2 [db gem]
+      (get-in db [gem :properties :hardness]))
+
+(println (durability gemstone-db :moissanite))
+(println (durability2 gemstone-db :moissanite))
