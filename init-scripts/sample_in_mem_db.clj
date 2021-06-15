490c6e9e437ca9545f1430fca15021617212eb6d
felipe-basina <felipe.basina@gmail.com>
2021-06-14 22:44:24 -0300

    Adicionado verificacao para nao permitir cadastrar registro existente; alterado funcao para retornar o conteudo completo quando consultado por parametro.

--- init-scripts/sample_in_mem_db.clj
+++ init-scripts/sample_in_mem_db.clj
@@ -25,12 +25,16 @@
 
 (defn insert [table record id-key]
       (let [updated-db (update-in (read-db) [table :data] conj record)
-            total-elements (count (get-in updated-db [table :data]))]
-           (write-db (update-in updated-db [table :indexes id-key] assoc (id-key record) (dec total-elements)))))
+            total-elements (count (get-in updated-db [table :data]))
+            content-exists? (contains? (get-in updated-db [table :indexes :name]) (id-key record))]
+           (if content-exists?
+             (println (str "Record with :name " (id-key record) " already exists. Aborting"))
+             (write-db (update-in updated-db [table :indexes id-key] assoc (id-key record) (dec total-elements))))))
 
 (insert :fruits {:name "Lemon" :stock 10} :name)
 (insert :fruits {:name "Grapefruit" :stock 12} :name)
 (insert :fruits {:name "Apple" :stock 28} :name)
+(insert :fruits {:name "Lemon" :stock 28} :name)
 (println (read-db))
 
 (defn select-* [table-name]
@@ -39,9 +43,9 @@
 (println (select-* :fruits))
 
 (defn select-*-where [table-name field field-value]
-      (let [element-index (get-in (read-db) [table-name :indexes :name field-value])]
-           (field (nth (get-in (read-db) [table-name :data]) element-index))))
+      (let [element-index (get-in (read-db) [table-name :indexes field field-value])]
+           (nth (get-in (read-db) [table-name :data]) element-index)))
 
 (println (select-*-where :fruits :name "Lemon"))
-(println (select-*-where :fruits :stock "Lemon"))
-(println (select-*-where :fruits :stock "Apple"))
+(println (select-*-where :fruits :name "Grapefruit"))
+(println (select-*-where :fruits :name "Apple"))
