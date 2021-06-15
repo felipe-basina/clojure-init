fefdb96ca3674dfd259f380c72e065cd290ebf57
felipe-basina <felipe.basina@gmail.com>
2021-06-14 21:14:44 -0300

    Adicionado funcoes para criar e remover tabela.

--- init-scripts/sample_in_mem_db.clj
+++ init-scripts/sample_in_mem_db.clj
@@ -7,3 +7,18 @@
 
 (defn write-db [new-db]
       (reset! memory-db new-db))
+
+(defn create-table [table-name]
+      (let [updated-db (assoc (read-db) (keyword table-name) {:data [] :indexes {}})]
+           (write-db updated-db)))
+
+(create-table "person")
+(create-table "fruits")
+(println (read-db))
+
+(defn drop-table [table-name]
+      (let [updated-db (dissoc (read-db) (keyword table-name))]
+           (write-db updated-db)))
+
+(drop-table "person")
+(println (read-db))
